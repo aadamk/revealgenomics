1ee14ae055e54b69eceb513b9e1ddb80974b4bea
Kriti Sen Sharma <ksen@paradigm4.com>
2018-04-03 12:53:33 -0400

    add wrapper for uploading expression matrix from client to server
    - register_expression_matrix_client allows upload from client to server
    - register_expression_matrix allows upload from server only (FASTER route)

---
+++ R/functions-measurement-upload.R
@@ -0,0 +1,307 @@
+#
+# BEGIN_COPYRIGHT
+#
+# PARADIGM4 INC.
+# This file is part of the Paradigm4 Enterprise SciDB distribution kit
+# and may only be used with a valid Paradigm4 contract and in accord
+# with the terms and conditions specified by that contract.
+#
+# Copyright (C) 2011 - 2017 Paradigm4 Inc.
+# All Rights Reserved.
+#
+# END_COPYRIGHT
+#
+
+# Functions to upload measurement data to SciDB
+
+#' Upload expression matrix file 
+#' 
+#' filepath must exist on SciDB server. Altrnatively use
+#' `register_expression_matrix_client`
+#' 
+#' @export
+register_expression_matrix = function(filepath,
+                                      measurementset_id,
+                                      featureset_id,
+                                      feature_type,
+                                      dataset_version = NULL,
+                                      only_test = FALSE,
+                                      con = NULL){
+  con = use_ghEnv_if_null(con)
+  
+  test_register_expression_matrix(filepath,
+                                  measurementset_id,
+                                  featureset_id,
+                                  feature_type,
+                                  dataset_version)
+  if (!only_test) {
+    if (is.null(dataset_version)) {
+      rqset = get_measurementsets(measurementset_id = measurementset_id, con = con) # finds the latest version
+      dataset_version = rqset$dataset_version
+      cat("Dataset version not specified. Inferred version from measurementset_id as version:", dataset_version, "\n")
+    } else {
+      stopifnot(length(dataset_version) == 1)
+      rqset = get_measurementsets(measurementset_id = measurementset_id,
+                                  dataset_version = dataset_version, con = con)
+    }
+    dataset_id = rqset$dataset_id
+    cat("Specified measurementset_id belongs to dataset:", dataset_id, "\n")
+    
+    arr_feature = full_arrayname(.ghEnv$meta$arrFeature)
+    arr_biosample = full_arrayname(.ghEnv$meta$arrBiosample)
+    arrayname = full_arrayname(.ghEnv$meta$arrRnaquantification)
+    cat("======\n")
+    cat(paste("Working on expression file:\n\t", filepath, "\n"))
+    x = read.delim(file = filepath, nrows = 10)
+    ncol(x)
+    
+    
+    ##################
+    # Do some simple checks on the column-names (only for debug purposes)
+    length(colnames(x))
+    length(grep(".*_BM", tail(colnames(x),-1) ))
+    length(grep(".*_PB", colnames(x) ))
+    
+    colnames(x)[grep(".*_2_.*", colnames(x) )]
+    
+    colnames(x)[grep(".*2087.*", colnames(x) )]
+    colnames(x)[grep(".*1179.*", colnames(x) )]
+    ##################
+    
+    
+    ############# START LOADING INTO SCIDB ########
+    
+    query = paste("aio_input('", filepath, "',
+                  'num_attributes=" , length(colnames(x)), "',
+                  'split_on_dimension=1')")
+    # t1 = scidb(con$db, query)
+    # t1 = con$db$between(srcArray = t1, lowCoord = "NULL, NULL, NULL, NULL", highCoord = R(paste("NULL, NULL, NULL,", length(colnames(x))-1)))
+    t0 = scidb(con$db, paste0("between(", query, ", NULL, NULL, NULL, NULL, NULL, NULL, NULL, ", length(colnames(x))-1, ")")) 
+    t1 = store(con$db, t0, temp=TRUE)
+    
+    # ================================
+    ## Step 1. Join the SciDB feature ID
+    
+    # first form the list of features in current table
+    # featurelist_curr = con$db$between(srcArray = t1, lowCoord = "NULL, NULL, NULL, 0", highCoord = "NULL, NULL, NULL, 0")
+    featurelist_curr = scidb(con$db, paste0("filter(", t1@name, ", attribute_no = 0)"))
+    cat(paste("number of feature_id-s in the current expression count table:", scidb_array_count(featurelist_curr, con = con)-1, "\n"))
+    FEATUREKEY = scidb(con$db, arr_feature)
+    cat(paste("number of feature_id-s in the SciDB feature ID list:", scidb_array_count(FEATUREKEY, con = con), "\n"))
+    
+    # sel_features = con$db$filter(FEATUREKEY, R(paste("feature_type='", feature_type, "' AND featureset_id = ", featureset_id, sep = "")))
+    sel_features = scidb(con$db, paste0("filter(", FEATUREKEY@name, ", ", 
+                                        "feature_type='", feature_type, "' AND featureset_id = ", featureset_id, ")"))
+    cat(paste("number of feature_id-s in the SciDB feature ID list for transcript type: '", feature_type,
+              "' and featureset_id: '", featureset_id, "' is:", scidb_array_count(sel_features, con = con), "\n", sep = ""))
+    
+    # ff = con$db$project(srcArray = sel_features, selectedAttr = "name")
+    ff = scidb(con$db, paste0("project(", sel_features@name, ", name)"))
+    
+    qq2 = paste0("equi_join(",
+                 ff@name, ", ",
+                 featurelist_curr@name, ", ",
+                 "'left_names=name', 'right_names=a', 'keep_dimensions=1')")
+    
+    qq2 = paste0("redimension(", qq2, 
+                 ", <feature_id :int64>[tuple_no=0:*,10000000,0,dst_instance_id=0:63,1,0,src_instance_id=0:63,1,0])")
+    
+    joinBack1 = scidb(con$db,
+                      paste("cross_join(",
+                            t1@name, " as X, ",
+                            qq2, " as Y, ",
+                            "X.tuple_no, Y.tuple_no, X.dst_instance_id, Y.dst_instance_id, X.src_instance_id, Y.src_instance_id)", sep = ""))
+    joinBack1@name
+    
+    joinBack1 = store(con$db, joinBack1, temp=TRUE)
+    
+    # Verify with
+    # scidb_array_head(con$db$between(srcArray = joinBack1, lowCoord = "0, NULL, NULL, NULL", highCoord = "0, NULL, NULL, NULL"))
+    
+    cat("Number of features in study that matched with SciDB ID:\n")
+    countFeatures = scidb_array_count(joinBack1) / ncol(x)
+    print(countFeatures)
+    
+    stopifnot(countFeatures == (scidb_array_count(featurelist_curr, con = con)-1))
+    # ================================
+    ## Step 2. Join the SciDB patient ID
+    # first form the list of patients in current table
+    
+    # patientlist_curr = con$db$between(t1, lowCoord = "0, 0, NULL, 1", highCoord = "0, 0, NULL, NULL")
+    patientlist_curr = scidb(con$db, paste0("between(", t1@name, ", 0, 0, NULL, 1, 0, 0, NULL, NULL)"))
+    # Check that the "public_id"_"spectrum_id" is unique enough, otherwise we have to consider the suffix "BM", "PB"
+    stopifnot(length(unique(as.R(patientlist_curr)$a)) == (ncol(x)-1))
+    
+    scidb_array_head(patientlist_curr, con = con)
+    
+    cat(paste("number of biosamples in the expression count table:", scidb_array_count(patientlist_curr, con = con), "\n"))
+    # PATIENTKEY = scidb(con$db, arr_biosample)
+    # PATIENTKEY = con$db$filter(PATIENTKEY, R(paste('dataset_id=', dataset_id)))
+    PATIENTKEY = scidb(con$db, paste0("filter(", arr_biosample, ", dataset_id = ", dataset_id, ")"))
+    cat(paste("number of biosamples registered in database in selected namespace:" , scidb_array_count(PATIENTKEY), "\n"))
+    
+    
+    # now do the joining
+    qq = paste("equi_join(",
+               patientlist_curr@name, ", ",
+               "project(filter(", PATIENTKEY@name, ", dataset_version = ", dataset_version, "), name), ",
+               "'left_names=a', 'right_names=name', 'keep_dimensions=1')")
+    joinPatientName = scidb(con$db, qq)
+    
+    cat("number of matching public_id-s between expression-count table and PER_PATIENT csv file:\n")
+    countMatches = scidb_array_count(joinPatientName)
+    print(countMatches)
+    
+    
+    
+    # Verify
+    # x1 = as.R(con$db$project(PATIENTKEY, "name"))
+    x1 = iquery(con$db, paste0("project(", PATIENTKEY@name, ", name)"), return = T)
+    x2 = as.R(patientlist_curr)
+    
+    stopifnot(countMatches == sum(x2$a %in% x1$name))
+    
+    # cat("The expression count table public_id-s that are not present in PER_PATIENT csv file: \n")
+    # tt = x2$public_id %in% x1$PUBLIC_ID
+    # print(x2$public_id[which(!tt)])
+    
+    # joinPatientName = con$db$redimension(joinPatientName,
+    #                                      R(paste("<biosample_id:int64>
+    #                                                 [attribute_no=0:", countMatches+1, ",",countMatches+2, ",0]", sep = "")))
+    qq3 = paste0("redimension(", joinPatientName@name,
+                 ", <biosample_id:int64>[attribute_no=0:", countMatches+1, ",",countMatches+2, ",0])")
+    
+    joinBack2 = scidb(con$db,
+                      paste0("cross_join(",
+                             joinBack1@name, " as X, ",
+                             qq3, "as Y, ",
+                             "X.attribute_no, Y.attribute_no)"))
+    joinBack2@name
+    
+    joinBack2 = store(con$db, joinBack2, temp=TRUE)
+    
+    # Verify with
+    # scidb_array_head(con$db$filter(joinBack2, "tuple_no = 0"))
+    
+    cat("Number of expression level values in current array:\n")
+    countExpressions = scidb_array_count(joinBack2)
+    print(countExpressions)
+    stopifnot(countExpressions == countMatches*countFeatures)
+    
+    ####################
+    # Redimension the expression level array
+    gct_table = scidb(con$db,
+                      paste("apply(",
+                            joinBack2@name, ", ",
+                            "value, dcast(a, float(null)), ",
+                            "measurementset_id, ", measurementset_id,
+                            ", dataset_id, ", dataset_id, 
+                            ", dataset_version, ", dataset_version,
+                            ")", sep = "")
+    )
+    
+    # Need to insert the expression matrix table into one big array
+    insertable_qq = paste0("redimension(", gct_table@name, ", ",
+                           arrayname, ")") # TODO: would be good to not have this resolution clash
+    
+    if (scidb_exists_array(arrayname, con = con)) {
+      cat(paste("Inserting expression matrix data into", arrayname, "at dataset_version", dataset_version, "\n"))
+      iquery(con$db, paste("insert(", insertable_qq, ", ", arrayname, ")"))
+    } else {
+      stop("expression array does not exist")
+    }
+    
+    remove_old_versions_for_entity(entitynm = .ghEnv$meta$arrRnaquantification, con = con)
+
+    return(measurementset_id)
+  } # end of if (!only_test)
+}
+
+#' @export
+register_expression_dataframe = function(df1, dataset_version, con = NULL){
+  con = use_ghEnv_if_null(con)
+  
+  test_register_expression_dataframe(df1)
+  
+  df1 = df1[, c('dataset_id', 'measurementset_id', 'biosample_id', 
+                'feature_id', 'value')]
+  df1 = plyr::rename(df1, c('value' = 'value__'))
+  
+  temp_arr_nm = paste0("temp_df_", stringi::stri_rand_strings(1, 6))
+  adf_expr0 = as.scidb_int64_cols(db = con$db,
+                                  df1 = df1,
+                                  int64_cols = c('dataset_id', 'measurementset_id', 'biosample_id', 
+                                                 'feature_id'),
+                                  chunk_size=nrow(df1), 
+                                  name = temp_arr_nm, 
+                                  use_aio_input = TRUE)
+  
+  qq2 = paste0("apply(", 
+               adf_expr0@name, 
+               ", value, float(value__)", 
+               ", dataset_version, ", dataset_version, ")")
+  
+  fullnm = full_arrayname(.ghEnv$meta$arrRnaquantification)
+  qq2 = paste0("redimension(", qq2, ", ", fullnm, ")")
+  
+  cat("inserting data for", nrow(df1), "expression values into", fullnm, 
+      "array at measurementset_id =", unique(df1$measurementset_id), "\n")
+  iquery(con$db, paste("insert(", qq2, ", ", fullnm, ")"))
+  iquery(con$db, paste0("remove(", temp_arr_nm, ")"))
+  
+  remove_old_versions_for_entity(entitynm = .ghEnv$meta$arrRnaquantification, con = con)
+}
+
+#' Upload expression matrix file 
+#' 
+#' filepath can exist on SciDB server or client
+
+#' `register_expression_matrix_client`
+#' 
+#' @export
+register_expression_matrix_client = function(filepath,
+                                             measurementset,
+                                             featureset,
+                                             biosample_ref = NULL,
+                                             feature_ref = NULL,
+                                             con = NULL) {
+  con = use_ghEnv_if_null(con = con)
+  
+  stopifnot(nrow(measurementset) == 1)
+  stopifnot(nrow(featureset) == 1)
+  
+  dataset_id = measurementset$dataset_id
+  dataset_version = measurementset$dataset_version
+  
+  if (is.null(feature_ref)) {
+    feature_ref = search_features(featureset_id = featureset$featureset_id) 
+  } else {
+    feature_ref   =   feature_ref[feature_ref$featureset_id == featureset$featureset_id, ]
+  }
+  if (is.null(biosample_ref)) {
+    biosample_ref = search_biosamples(dataset_id = dataset_id)
+  } else {
+    biosample_ref = biosample_ref[biosample_ref$dataset_id ==  dataset_id, ] 
+  }
+  
+  expr_df = read.delim(file = expr_file_path, sep = "\t")
+  expr_df = tidyr::gather(data = expr_df,
+                          key = 'biosample_name',
+                          value='value',
+                          colnames(expr_df)[2:length(colnames(expr_df))])
+  
+  expr_df$feature_id =   feature_ref[match(expr_df$GENE_ID, feature_ref$name), ]$feature_id
+  if (any(is.na(expr_df$feature_id))) {
+    stop("All features in expression matrix file (row names) must have been uploaded before to the featureset: ",
+         featureset$featureset_id)
+  }
+  expr_df$biosample_id = biosample_ref[match(expr_df$biosample_name, biosample_ref$name), ]$biosample_id
+  if (any(is.na(expr_df$biosample_id))) {
+    stop("All samples in expression matrix file (column names) must have been uploaded before to study / dataset id: ",
+         dataset_id)
+  }
+  expr_df$dataset_id = dataset_id
+  expr_df$measurementset_id = measurementset_id
+  register_expression_dataframe(df1 = expr_df, dataset_version = 1, con = con)
+  
+}
