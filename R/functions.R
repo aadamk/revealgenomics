a0e17fa11bd48570005aa8b7333e089a718f6718
Kriti Sen Sharma <ksen@paradigm4.com>
2018-05-21 13:38:33 -0400

    remove use of deprecated dao_get_ENTITY() call

--- R/functions.R
+++ R/functions.R
@@ -191,7 +191,7 @@ get_biosamples_from_cache = function(updateCache = FALSE, con = NULL){
 
 update_biosample_cache = function(con = NULL){
   con = use_ghEnv_if_null(con)
-  .ghEnv$cache$biosample_ref = dao_get_biosample(con = con)
+  .ghEnv$cache$biosample_ref = get_biosamples(con = con)
 }
 
 get_ontology_from_cache = function(updateCache = FALSE, con = NULL){
