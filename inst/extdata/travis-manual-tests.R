e58165fa9560344adc8b763246bdda95b0d48554
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-18 15:34:33 -0400

    fix bugs in travis tests

--- inst/extdata/travis-manual-tests.R
+++ inst/extdata/travis-manual-tests.R
@@ -1,5 +1,6 @@
 # to be run in Travis after `04-load-api.Rmd` has been run
-
+library(scidb4gh)
+gh_connect()
 stopifnot(nrow(get_datasets()) == 1)
-stopifnot(nrow(get_experiment()) == 0)
+stopifnot(nrow(get_experimentset()) == 1)
 stopifnot(nrow(get_experiment()) == 0)
