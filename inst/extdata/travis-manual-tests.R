67af7a3c78f88e45de6f9c2789764cb2ffc9b2fb
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-24 22:37:45 -0400

    add tests via notebooks
    - notebook for update-delete,
    - notebook for loading-experiment-measurement
    - update travis tests accordingly

--- inst/extdata/travis-manual-tests.R
+++ inst/extdata/travis-manual-tests.R
@@ -1,18 +1,20 @@
-# to be run in Travis after `04-load-api.Rmd` has been run
+#' to be run in Travis after the following notebooks have been run
+#' - `04-load-api.Rmd`
+#' - `06-updating-deleting.Rmd`
+#' - `07-experiments-measurements.Rmd`
 library(scidb4gh)
 gh_connect()
-populate_measurements()
 stopifnot(nrow(get_datasets()) == 1)
 stopifnot(nrow(get_experimentset()) == 1)
 stopifnot(nrow(get_experiments()) == 3)
 stopifnot(nrow(get_measurements()) == 3)
 
 # check mandatory fields flag
-stopifnot(all(dim(get_datasets()) == c(1, 9)))
+stopifnot(all(dim(get_datasets()) == c(1, 10)))
 stopifnot(all(dim(get_datasets(mandatory_fields_only = T)) == c(1, 8)))
 stopifnot(all(dim(get_individuals()) == c(3, 10)))
 stopifnot(all(dim(get_individuals(mandatory_fields_only = T)) == c(3, 9)))
-stopifnot(all(dim(get_biosamples()) == c(3, 10)))
+stopifnot(all(dim(get_biosamples()) == c(3, 11)))
 stopifnot(all(dim(get_biosamples(mandatory_fields_only = T)) == c(3, 9)))
 
 stopifnot(all(dim(get_measurements()) == c(3, 11)))
