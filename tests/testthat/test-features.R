6c3abd3db326bf8f3ea222677182223249c600ee
Kriti Sen Sharma <ksen@paradigm4.com>
2018-07-06 17:24:13 -0400

    need to initialize Features before calling get_features)
    - fixes broken travis build: https://travis-ci.org/Paradigm4/reveal-genomics/builds/401056888

---
+++ tests/testthat/test-features.R
@@ -0,0 +1,30 @@
+context("test-download.R")
+
+test_that("Check that feature download works when feature_id is a long integer ", {
+  # cat("# Now connect to scidb\n")
+  e0 = tryCatch({gh_connect()}, error = function(e) {e})
+  if (!("error" %in% class(e0))) { # do not run this on EE installs, mainly targeted for Travis
+    init_db(arrays_to_init = c(.ghEnv$meta$arrFeature, 
+                               .ghEnv$meta$arrGeneSymbol), force = TRUE)
+    feature_id = register_feature(df = data.frame(name = "dummyFeature", 
+                                     gene_symbol = "dummySymbol",
+                                     featureset_id = 1000, 
+                                     chromosome="...", 
+                                     start="...", 
+                                     end="...", 
+                                     strand_term = 1, 
+                                     feature_type = "gene", 
+                                     source = "...", 
+                                     stringsAsFactors = FALSE))
+    # https://github.com/Paradigm4/reveal-genomics/issues/40
+    ff = get_features(feature_id = seq(70000,800000,10))
+    expect_true(nrow(ff) == 0) # in tests, currently do not expect to upload 700K+ features
+    
+    expect_try(nrow(get_features()) == 1)
+    
+    # Clean-up
+    init_db(arrays_to_init = c(.ghEnv$meta$arrFeature, 
+                               .ghEnv$meta$arrGeneSymbol), force = TRUE)
+    
+  }
+})
