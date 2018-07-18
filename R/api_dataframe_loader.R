a89bbfa7a9d13aa81c59b608059a3fc402976b73
Kriti Sen Sharma <ksen@paradigm4.com>
2018-07-18 15:04:56 -0400

    when assigning biosample ids, only compare with relevant biosamples
    - using pipeline information, can infer whether sample should be of "__RNA" or "__DNA" type. Use this information to downselect biosample list to only relevant samples
    - allow slot for future suffixes

--- R/api_dataframe_loader.R
+++ R/api_dataframe_loader.R
@@ -21,8 +21,20 @@ DataLoader = R6::R6Class(classname = "DataLoader",
           print_level = function() {cat("----(Level: DataLoader)\n")},
           assign_biosample_ids = function(){
             cat("assign_biosample_ids()"); self$print_level()
-            
             bios_ref = private$.reference_object$biosample
+            if (private$.reference_object$measurement_set$entity %in% c(.ghEnv$meta$arrRnaquantification)) {
+              suffix = "__RNA"
+            } else if (private$.reference_object$measurement_set$entity %in% 
+                       c(.ghEnv$meta$arrVariant,
+                         .ghEnv$meta$arrFusion)) {
+              suffix = "__DNA"
+            } else {
+              stop("Assigning suffix '__DNA' or '__RNA' based on entity type. 
+                   Suffix needs to be assigned for current entity:", private$.reference_object$measurement_set$entity)
+            }
+            bios_ref = bios_ref[grep(suffix, bios_ref$name), ]
+            cat("Chose suffix:", suffix, "for entity:", private$.reference_object$measurement_set$entity, 
+                "\nRetained:", nrow(bios_ref), "of total:", nrow(private$.reference_object$biosample), "in manifest\n")
             if (nrow(private$.reference_object$pipeline_df) > 1) { 
               # multiple Measurements combined into one file
               # ==> data must contain a column called `biosample_name`
