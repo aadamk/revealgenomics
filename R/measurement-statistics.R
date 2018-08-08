101299481d4370065237b7faf2109d6944e235fd
Kriti Sen Sharma <ksen@paradigm4.com>
2018-08-07 23:54:11 -0400

    aggregate by pheno accounting for molecule type
    - return counts of project, study by pheno, split by sample_molecule_type

--- R/measurement-statistics.R
+++ R/measurement-statistics.R
@@ -164,68 +164,170 @@ calculate_statistics_across_measurementsets = function(df_measurementset, con = 
 #'           529      238             1          1 MMRF CoMMpass MMRF CoMMpass IA14a primary_disease   multiple myeloma  2072
 #' @export           
 aggr_proj_study_by_pheno = function(
-  filter_column, con = NULL
+  filter_column, method = 1, con = NULL
 ) {
   con = revealgenomics:::use_ghEnv_if_null(con = con)
   query0 = paste0("key='", filter_column, "'", collapse = " OR ")
-  query1 = paste0(
-    "grouped_aggregate(
-    filter(",
-    revealgenomics:::full_arrayname(.ghEnv$meta$arrBiosample), "_INFO, ",
-    query0, "), 
-    min(key) AS filter_column, 
-    count(*) AS total, 
-    dataset_id, dataset_version, val)"
-  )
-  query2 = paste0(
-    "equi_join(",
-    query1, ", ", 
-    "project(apply(", 
-    revealgenomics:::full_arrayname(.ghEnv$meta$arrDataset), ", ",
-    "study_name, name), study_name, project_id), ",
-    "'left_names=dataset_id,dataset_version', 
-    'right_names=dataset_id,dataset_version', 'keep_dimensions=0'",
-    ")"
-  )
-  query3 = paste0(
-    "equi_join(",
-    query2, ", ", 
-    "project(apply(", 
-    revealgenomics:::full_arrayname(.ghEnv$meta$arrProject), ", ",
-    "project_name, name), project_name), ",
-    "'left_names=project_id', 
+  if (method == 1) {
+    query1 = paste0(
+      "grouped_aggregate(
+      filter(",
+      revealgenomics:::full_arrayname(.ghEnv$meta$arrBiosample), "_INFO, ",
+      query0, "), 
+      min(key) AS filter_column, 
+      count(*) AS total, 
+      dataset_id, dataset_version, val)"
+    )
+    query2 = paste0(
+      "equi_join(",
+      query1, ", ", 
+      "project(apply(", 
+      revealgenomics:::full_arrayname(.ghEnv$meta$arrDataset), ", ",
+      "study_name, name), study_name, project_id), ",
+      "'left_names=dataset_id,dataset_version', 
+      'right_names=dataset_id,dataset_version', 'keep_dimensions=0'",
+      ")"
+    )
+    query3 = paste0(
+      "equi_join(",
+      query2, ", ", 
+      "project(apply(", 
+      revealgenomics:::full_arrayname(.ghEnv$meta$arrProject), ", ",
+      "project_name, name), project_name), ",
+      "'left_names=project_id', 
     'right_names=project_id', 
     'keep_dimensions=0'",
-    ")"
-  )
-  
-  res = iquery(con$db, 
-               query3, 
-               return = T, 
-               only_attributes=T)
-  res = plyr::rename(res, 
-                     c("dataset_id"      = "study_id",
-                       "dataset_version" = "study_version", 
-                       "val"             = "filter_value"))
-  def = get_definitions(con = con)
-  res2 = merge(
-    res, 
-    def[, c('dataset_id', 'attribute_name', 'controlled_vocabulary')], 
-    by.x = c('study_id', 'filter_column'), 
-    by.y = c('dataset_id', 'attribute_name'), 
-    all.x = TRUE)
-  
-  controlled_idx = which(!is.na(res2$controlled_vocabulary))
-  if (length(controlled_idx) > 0) {
-    controlled_idx_ont_ids = res2[controlled_idx, 'filter_value']
-    ont_df = get_ontology(ontology_id = unique(controlled_idx_ont_ids), con = con)
-    res2[controlled_idx, 'filter_value'] = 
-      ont_df[match(controlled_idx_ont_ids, ont_df$ontology_id), ]$term
+      ")"
+    )
+    
+    res = iquery(con$db, 
+                 query3, 
+                 return = T, 
+                 only_attributes=T)
+    res = plyr::rename(res, 
+                       c("dataset_id"      = "study_id",
+                         "dataset_version" = "study_version", 
+                         "val"             = "filter_value"))
+    def = get_definitions(con = con)
+    res2 = merge(
+      res, 
+      def[, c('dataset_id', 'attribute_name', 'controlled_vocabulary')], 
+      by.x = c('study_id', 'filter_column'), 
+      by.y = c('dataset_id', 'attribute_name'), 
+      all.x = TRUE)
+    
+    controlled_idx = which(!is.na(res2$controlled_vocabulary))
+    if (length(controlled_idx) > 0) {
+      controlled_idx_ont_ids = res2[controlled_idx, 'filter_value']
+      ont_df = get_ontology(ontology_id = unique(controlled_idx_ont_ids), con = con)
+      res2[controlled_idx, 'filter_value'] = 
+        ont_df[match(controlled_idx_ont_ids, ont_df$ontology_id), ]$term
+    }
+    # res2$controlled_vocabulary = NULL
+    res2 = res2[, c('study_id', 'study_version', 'project_id', 'project_name', 'study_name',
+                    'filter_column', 'filter_value', 'total')]
+    
+    return(res2)
+  } else if (method == 2) {
+    if ('sample_molecule_type' %in% filter_column) {
+      stop("`sample_molecule_type` as a reserved column. 
+         Do not use as filter_column")
+    }
+    query1 = paste0(
+      "grouped_aggregate(
+        filter(
+          equi_join(
+            apply(", revealgenomics:::full_arrayname(.ghEnv$meta$arrBiosample), 
+              "_INFO, dataset_id, dataset_id, dataset_version, dataset_version), 
+            project(
+              apply(
+                filter(", revealgenomics:::full_arrayname(.ghEnv$meta$arrBiosample),
+                  "_INFO, key='sample_molecule_type'), 
+                sample_molecule_type, val), 
+              sample_molecule_type), 
+          'left_names=biosample_id', 
+          'right_names=biosample_id', 
+          'left_outer=true'), ",
+        query0, 
+        "), 
+      count(*) AS total, 
+      dataset_id, dataset_version, key, val, sample_molecule_type)"
+    )
+    query2 = paste0(
+      "equi_join(",
+      query1, ", ", 
+      "project(apply(", 
+      revealgenomics:::full_arrayname(.ghEnv$meta$arrDataset), ", ",
+      "study_name, name), study_name, project_id), ",
+      "'left_names=dataset_id,dataset_version', 
+      'right_names=dataset_id,dataset_version', 'keep_dimensions=0'",
+      ")"
+    )
+    query3 = paste0(
+      "equi_join(",
+      query2, ", ", 
+      "project(apply(", 
+      revealgenomics:::full_arrayname(.ghEnv$meta$arrProject), ", ",
+      "project_name, name), project_name), ",
+      "'left_names=project_id', 
+    'right_names=project_id', 
+    'keep_dimensions=0'",
+      ")"
+    )
+    res = iquery(con$db, 
+           query = query3,
+           return = T,
+           only_attributes=T)
+    
+    res = plyr::rename(res, 
+                       c("dataset_id"      = "study_id",
+                         "dataset_version" = "study_version", 
+                         "val"             = "filter_value",
+                         "key"             = "filter_column"))
+    def = get_definitions(con = con)
+    res2 = merge(
+      res, 
+      def[, c('dataset_id', 'attribute_name', 'controlled_vocabulary')], 
+      by.x = c('study_id', 'filter_column'), 
+      by.y = c('dataset_id', 'attribute_name'), 
+      all.x = TRUE)
+    controlled_idx = which(!is.na(res2$controlled_vocabulary))
+    if (length(controlled_idx) > 0) {
+      controlled_idx_ont_ids = res2[controlled_idx, 'filter_value']
+      ont_df = get_ontology(ontology_id = unique(controlled_idx_ont_ids), con = con)
+      res2[controlled_idx, 'filter_value'] = 
+        ont_df[match(controlled_idx_ont_ids, ont_df$ontology_id), ]$term
+    }
+    
+    ont_df = get_ontology(ontology_id = unique(res2$sample_molecule_type), con = con)
+    res2$sample_molecule_type = 
+      ont_df[match(res2$sample_molecule_type, ont_df$ontology_id), ]$term
+    res2$controlled_vocabulary = NULL
+    head(res2)
+    res2 = res2[, c('study_id', 'study_version', 'project_id', 'project_name', 'study_name',
+                    'filter_column', 'filter_value', 'sample_molecule_type', 'total')]
+    
+    return(res2)
+    
+    if (FALSE) {
+      # code to add `sample_molecule_type = NA` when sample not tagged`
+      ont_df = get_ontology(con = con)
+      ont_df[grep("sample_molecule_type", ont_df$category), c("ontology_id", "term", "category")]
+      #    ontology_id         term             category
+      # 66          66          RNA sample_molecule_type
+      # 67          67         mRNA sample_molecule_type
+      # 68          68          DNA sample_molecule_type
+      # 69          69 blood_plasma sample_molecule_type
+      # 70          70           BP sample_molecule_type
+      # 71          71           NA sample_molecule_type      
+      res0 = iquery(con$db, "limit(op_count(merge(redimension(project(apply(aggregate(gh_secure.BIOSAMPLE_INFO, count(*), dataset_version, dataset_id, biosample_id), key_id, int64(10000), sample_molecule_type, string(-10001)), key_id, sample_molecule_type),<sample_molecule_type:string> [dataset_version=0:*:0:100; dataset_id=0:*:0:10; biosample_id=0:*:0:100; key_id=0:*:0:242]),
+       
+       project(apply(filter(gh_secure.BIOSAMPLE_INFO, key='sample_molecule_type'), sample_molecule_type, val), sample_molecule_type))), 5)", return = T)
+    }
   }
-  # res2$controlled_vocabulary = NULL
-  res2 = res2[, c('study_id', 'study_version', 'project_id', 'project_name', 'study_name',
-                  'filter_column', 'filter_value', 'total')]
-  
-  return(res2)
 }
 
+aggr_proj_study_by_pheno_v2 = function(
+  filter_column, con = NULL
+) {
+}
