8f752d13a8d430a2b8ba2f3a098a93ab7240f967
Kriti Sen Sharma <ksen@paradigm4.com>
2018-11-30 18:26:09 +0530

    add tests for updation of entities via Excel loader
    - to test against #52
    - also register reference featureset at grch38

--- tests/testthat/test-zzzLast-api-loader.R
+++ tests/testthat/test-zzzLast-api-loader.R
@@ -27,6 +27,7 @@ test_that("Register entities via workbook works OK", {
     register_entities_workbook(workbook = wb, 
                                register_upto_entity = 'MEASUREMENTSET')
     
+    ##### Spot checks on Dataset #####
     # Spot checks that ontology fields of study have been captured properly
     datasets = get_datasets()
     expect_true(all(!is.na(datasets$study_type)))
@@ -44,10 +45,77 @@ test_that("Register entities via workbook works OK", {
     expect_true(all(datasets$DAS %in% wb$Studies$DAS))
     expect_true(all(datasets$study_type %in% wb$Studies$study_type))
     
+    ######## Update Study ##########
+    wb$Studies$URL[2] = "https://clinicaltrials.gov/ct2/show/zzzz" # edit an existing field
+    wb$Studies$new_field1 = c('entry1', 'entry2') # add a new column
+    register_entities_workbook(workbook = wb, 
+                               register_upto_entity = 'DEFINITION', 
+                               entity_to_update = 'DATASET')
+    datasets = get_datasets()
+    expect_true(length(grep("zzzz", datasets$URL)) ==1)
+    expect_true(all(datasets$new_field1 %in% c('entry1', 'entry2')))
+    
+    ######## Update Individual ##########
+    ## Edit an entry
+    rand_idx = 1
+    test_disease_str = 'test disease 1'
+    wb$Subjects[rand_idx, ]$primary_disease = test_disease_str
+    
+    ## Add a new column
+    rand_string = stringi::stri_rand_strings(nrow(wb$Subjects), length = 6)
+    wb$Subjects$new_field1 = rand_string
+    # Introduce new definition for the new field
+    new_definition = wb$Definitions[wb$Definitions$attribute_name == 'subject_id', ]
+    new_definition$attribute_name = 'new_field1'
+    new_definition$attribute_in_Samples = FALSE
+    wb$Definitions = rbind(
+      wb$Definitions, 
+      new_definition
+      )
+    register_entities_workbook(workbook = wb, 
+                               register_upto_entity = 'INDIVIDUAL', 
+                               entity_to_update = 'INDIVIDUAL')
+    
+    # Spot checks for the updates
+    ii = get_individuals()
+    expect_true(all(ii$new_field1 %in% rand_string))
+    expect_equal(
+      length(which(ii$primary_disease == test_disease_str)),
+      1)
+    
+    ######## Update MeasurementSet (trickier) ##########
+    ## Edit an entry related to `pipeline_choices` sheet;
+    # `pipeline_applications` column: "Defuse" ==> "deFuse"
+    wb$pipeline_choices[wb$pipeline_choices$pipeline_scidb 
+                        == '[external]-[Fusion] Defuse', 
+                        ]$pipeline_applications = "deFuse"
+    # Edit an entry related to `filter_choices` sheet;
+    # `filter_description2` column 
+    rand_string2 = stringi::stri_rand_strings(n = 1, length = 10)
+    filter_name = 'gene - counts'
+    wb$filter_choices[wb$filter_choices$filter_name 
+                        == filter_name, 
+                        ]$filter_description2 = rand_string2
+    register_entities_workbook(workbook = wb, 
+                               register_upto_entity = 'MEASUREMENTSET', 
+                               entity_to_update = 'MEASUREMENTSET')
+    
+    # Spot checks
+    ms = get_measurementsets()
+    expect_equal(
+      length(which(ms$pipeline_applications == "deFuse")),
+      1)
+    expect_true(
+      all(ms[ms$filter_name == filter_name, ]$filter_description2 == rand_string2))
+    
+    ##### FeatureSet #####
     # Build up a featureset to be used for loading data
     fsets = get_featuresets()
     stopifnot(nrow(fsets) == 2)
     target_featureset_id = fsets[grep("37", fsets$name), ]$featureset_id
+    ftr_record = build_reference_gene_set(featureset_id = target_featureset_id)  # why is ftr_record unused?
+    
+    target_featureset_id = fsets[grep("38", fsets$name), ]$featureset_id
     ftr_record = build_reference_gene_set(featureset_id = target_featureset_id)  # why is ftr_record unused?
     
     ########### FUSION DATA ############
