3a94f2acd564f846011d44149fa362205c752340
Kriti Sen Sharma <ksen@paradigm4.com>
2019-02-15 02:12:45 -0500

    add function to register msmtdata binary cache

---
+++ R/functions_binary.R
@@ -0,0 +1,58 @@
+#' @export
+register_measurementdata = function(
+  measurementset, 
+  pipeline_data, 
+  con = NULL
+) {
+  con = revealgenomics:::use_ghEnv_if_null(con = con)
+  fullnm = full_arrayname(.ghEnv$meta$arrMeasurementData)
+  
+  # Begin uploading
+  payoad_class = as.character(class(pipeline_data))
+  pipeline_data = serialize(pipeline_data, NULL)
+  payload_size = as.numeric(object.size(pipeline_data))
+  THRESH_SCIDB_BINARY_UPLOAD = 900*1024*1024 # 900 MB at a time
+  factor = ceiling(payload_size / THRESH_SCIDB_BINARY_UPLOAD)
+  
+  payload_length = length(pipeline_data) 
+  stepsize = ceiling(payload_length/factor)
+  for (idx in 1:factor) {
+    cat("idx:", idx, "of", factor, "\n")
+    
+    if (idx < factor) {
+      range_idx = c((stepsize*(idx-1) + 1): (stepsize*idx))
+    } else { # remember to include till the very end 
+      range_idx = c((stepsize*(idx-1) + 1): payload_length)
+    }
+    pipeline_data_db = as.scidb(
+      con$db, 
+      pipeline_data[range_idx])
+    # Rename as binary `payload`
+    qq1 = paste0("cast(", pipeline_data_db@name, ", <payload:binary>[i])")
+    # Add other attribute values
+    qq1 = paste0(
+      "apply(", 
+      qq1, ", ", 
+      "measurementset_id, ", measurementset$measurementset_id, ", ", 
+      "dataset_id, ", measurementset$dataset_id, ", ", 
+      "dataset_version, ", measurementset$dataset_version, ", ", 
+      "subpart_id, ", idx, ", ", 
+      "payload_size_bytes, int64(", payload_size, "), ", 
+      "payload_class, '", payoad_class, "')"
+    )
+    # Redimension into target schema and insert
+    qq1 = paste0(
+      "redimension(", 
+      qq1, 
+      ", ", fullnm, ")"
+    )
+    qq1 = paste0(
+      "insert(", 
+      qq1, ", ", fullnm, ")"
+    )
+    cat("inserting data of size", payload_size/factor/1024/1024, "MB into", fullnm, 
+        "array at measurementset_id =", unique(measurementset$measurementset_id), "\n")
+    
+    iquery(con$db, qq1)
+  }
+}
