6844c1ded1e94140fbe00767aa5de5bcc6c3f27d
mmccorry <mmccorry@paradigm4.com>
2019-04-15 18:25:42 +0000

    Add console output during long SciDB load

--- R/search.R
+++ R/search.R
@@ -682,7 +682,9 @@ dao_search_expression = function(entity,
     #       Post an issue at https://github.com/Paradigm4/revealgenomics/issues\n")
     #   return(NULL)
     # }
+    cat("Downloading full dataset from SciDB ... ")
     res = iquery(con$db, query = qq3, binary = TRUE, only_attributes = TRUE, return = TRUE)
+    cat(" done.\n")
   }
   
   if (!formExpressionSet) return(res)
